644f2d272b88091c1f5bc94f1956e44fb99ba852
VertexC <bob2420083992@gmail.com>
2018-02-05 23:11:17 -0800

    set up 1) RootLayer.fxml 2) VideoPlayer.fxml

---
+++ src/core/view/VideoPlayerController.java
@@ -0,0 +1,14 @@
+package core.view;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class VideoPlayerController {
+    @FXML
+    private Button button;
+
+    @FXML
+    private ImageView currentFrame;
+}
