1d08dd803642310535ae1513f57e1f554d916f9d
VertexC <bob2420083992@gmail.com>
2018-02-07 20:11:50 -0800

    Create the file chooser successfully, not load file yet

--- src/core/view/RootLayoutController.java
+++ src/core/view/RootLayoutController.java
@@ -1,9 +1,29 @@
 package core.view;
 
+import com.sun.tools.javac.Main;
 import core.MainApp;
 import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class RootLayoutController {
 
+    private Stage rootStage;
+    private MainApp mainApp;
+
+    public void setRootStage(Stage stage){
+        this.rootStage = stage;
+    }
+
+    public void setMainApp(MainApp mainApp){
+        this.mainApp = mainApp;
+    }
+    @FXML
+    private void handleImport(){
+        Stage dialogStage = new Stage();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open the Source File");
+        fileChooser.showOpenDialog(dialogStage);
+    }
 }
